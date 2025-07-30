eac52399000ffd4bcef7ac2c795dc3a5bfda7445
ekyrizky <m.rizkyananda57@gmail.com>
2025-07-30 13:59:56 +0700

    update theme: typography

--- complay/src/main/java/com/ekyrizky/complay/designsystem/theme/Theme.kt
+++ complay/src/main/java/com/ekyrizky/complay/designsystem/theme/Theme.kt
@@ -10,11 +10,13 @@ import androidx.compose.ui.Modifier
 @Composable
 fun ComplayTheme(
     colors: ComplayColors = ComplayColors.defaultColors(),
+    typography: ComplayTypography = ComplayTypography.defaultTypography(),
     content: @Composable () -> Unit
 ) {
 
     CompositionLocalProvider(
         LocalColors provides colors,
+        LocalTypography provides typography
     ) {
         Box(
             modifier = Modifier.background(colors.background)
@@ -29,4 +31,9 @@ object ComplayTheme {
         @Composable
         @ReadOnlyComposable
         get() = LocalColors.current
+
+    val typography: ComplayTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTypography.current
 }
